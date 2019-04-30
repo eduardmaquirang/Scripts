e1db2a48d58924ffe0e13acb815bcf47cece2123
eduardmaquirang <50131078+eduardmaquirang@users.noreply.github.com>
2019-04-30 14:21:28 +0800

    Add files via upload

---
+++ samplecode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Update apt cache.
+sudo apt-get update
+
+# Install Nginx.
+sudo apt-get install -y nginx
+
+# Variable
+user: "Eduard Maquirang"
+
+# Set the home page.
+echo "<html><body><h2>Welcome to Azure! Virtual Machine Name is $(hostname).</h2><br><h3>Created by: $(user)</h3></body></html>" | sudo tee -a /var/www/html/index.html
